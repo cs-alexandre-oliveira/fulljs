ed59e8a68f5d9b70614e3c535e40e33271ecad95
Alexandre DEV <calexandredev@gmail.com>
2018-05-27 18:25:19 -0300

    working with sass, test data, fetching data from a remote api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
+    contests: []
   };
   componentDidMount() {
-    // timers, listeners
+    axios.get('/api/contests')
+      .then(resp => {
+        this.setState({
+          contests: resp.data.contests
+        });
+      })
+      .catch(console.error);
   }
   componentWillUnmount()  {
     // clean timers, listeners
@@ -16,7 +25,9 @@ class App extends React.Component {
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          -----
+          {this.state.contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest} />
+          )}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
+import data from './testData';
 
 ReactDOM.render(
   <App />,
